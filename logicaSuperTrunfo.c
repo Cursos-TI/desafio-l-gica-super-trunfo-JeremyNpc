bfaa194dbfed0fb9f91d422c5ef9747e8f390780
JeremyNpc <guilhermes3684@gmail.com>
2025-03-02 13:49:31 +0000

    começo do nivel2

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
+int main() {         
     
     
 char R;
@@ -13,9 +15,6 @@ int Turismo =30;
 float Pibpc1 = Pib1 / populacao;
 float Dp1 =  populacao / Area1;
 
-printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n Populaçao: %d habitantes\n ,Area: %f km\n, Pib: %f\n, Pontos_Turisticos: %d\n ", R, Codigoc1, Cidade1, populacao, Area1, Pib1, Turismo);
-printf("Densidade Populacional: %f\n", Dp1);
-printf("Pib per capita: %f\n\n", Pibpc1);
 
 char S;
 char codigoc[20] ="SP01";
@@ -27,45 +26,53 @@ int Pt = 50;
 float Pibpc2 =pib / Populacao;
 float Dp2 = Populacao / Area2;
 
+int Menu;
+
+
+
+printf("***Super Trunfo***\n\n");
+printf("1. Iniciar\n");
+printf("2. Ver regras\n");
+printf("3. Ver as cartas\n");
+printf("4. Sair\n");
+scanf("%d", &Menu);
+
+
+
+switch(Menu){
+case 1:
+printf("escolha entre carta1 e carta2\n");
+printf("1.Rio de janeiro\n 2.São Paulo\n");
+break;
+switch(Menu){
+    case 1:
+    printf("Escolha um atributo: \n 1.População\n 2.Area\n 3.Pib\n 4.Pontos turisticos\n 5.Densidade Populacional\n 6.Pib per capita");
+    
+}
+
+break;
+case 2:
+printf("As regras são:A carta com maior valor vence menos densidade populacional a qual e o menor valor que vence");
+break;
+case 3:
+printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n Populaçao: %d habitantes\n ,Area: %f km\n, Pib: %f\n, Pontos_Turisticos: %d\n ", R, Codigoc1, Cidade1, populacao, Area1, Pib1, Turismo);
+printf("Densidade Populacional: %f\n", Dp1);
+printf("Pib per capita: %f\n\n", Pibpc1);
+
 printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n, Populaçao: %d habitantes\n, Area: %f km\n, Pib: %f\n, Pontos_Turisticos: %d ", S, codigoc, cidade2, populacao, Area2, pib, Pt);
 printf("Densidade Populacional: %f\n", Dp2);
 printf("Pib per capita: %f\n\n", Pibpc2);
+break;
+case 4:
+printf("Saindo****");
+break;
+default:
+printf("Opção invalida");
 
-if(populacao > Populacao){
-    printf("Carta1: venceu em população\n");
-}else{
-    printf("Carta2: venceu em populção\n");
+
 }
 
-if(Area1 > Area2){
-    printf("Carta1: venceu em area\n");
-}else{
-    printf("Carta2: venceu em area\n");
-}
 
-if(Pib1 > pib){
-    printf("Carta1: venceu em pib\n");
-}else{
-    printf("Carta2: venceu em pib\n");
-}
 
-if(Turismo > Pt){
-    printf("Carta1: venceu em maior quantidade pontos turisticos\n");
-}else{
-    printf("Carta2: venceu em maior quantidade de pontos turisticos\n");
-}
 
-if(Pibpc1 > Pibpc2){
-    printf("Carta1: venceu em pib per capita\n");
-}else{
-    printf("Carta2: venceu em pib per capita\n");
-}
-
-if(Dp1 < Dp2){
-    printf("Carta1: venceu em densidade populacional\n");
-}else{
-    printf("Carta2: venceu em densidade populacional\n");
-}
-
-    return 0;
 }
